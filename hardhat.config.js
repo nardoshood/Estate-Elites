b95af8fa9a52a18fc0b5ebb2fcd96a0f9259507f
Nardos Tilahun <nardostilahun83@gmail.com>
2023-05-19 01:44:13 +0300

    updated config files and deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 require('dotenv').config({path: '.env'});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  networks:{
-    alfajores:{
-      url:"https://alfarajores-forno.celo-testnet.org",
-      accounts:{
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: {
         mnemonic: process.env.MNEMONIC,
         path: "m/44'/52752'/0'/0",
-    
-    },
-    chainId: 44787,
+      },
+      chainId: 44787,
+      gas: 6000000,
   },
 },
 };
